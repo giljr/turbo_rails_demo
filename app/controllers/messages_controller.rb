c1655ca2bfb44d4765aa3eaf4650b152627eee0a
jaythree <giljr.2009@gmail.com>
2023-08-26 14:50:08 -0400

    :recycle: update CRUD

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,10 @@
 class MessagesController < ApplicationController
   before_action :set_room, only: %i[new create]
 
+  def index
+    @messages = @room.messages
+  end
+
   def new
     @message = @room.messages.new
   end
@@ -11,6 +15,31 @@ class MessagesController < ApplicationController
     respond_to do |format|
       format.turbo_stream
       format.html { redirect_to @room }
+    end
+  end
+
+  def edit
+    @message = @room.messages.find(params[:id])
+  end
+
+  def update
+    @message = @room.messages.find(params[:id])
+    if @message.update(message_params)
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to room_messages_path(@room) }
+      end
+    else
+      # Handle validation errors
+    end
+  end
+
+  def destroy
+    @message = @room.messages.find(params[:id])
+    @message.destroy
+    respond_to do |format|
+      format.turbo_stream { render turbo_stream: turbo_stream.remove(@message) }
+      format.html { redirect_to room_messages_path(@room) }
     end
   end
 
